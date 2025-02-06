afebce5d9ee587b1f6b114ed15cd83868c51bba0
GuhAlcantara <Gustavoamv@gmail.com>
2025-02-06 13:53:01 +0000

    Estruturando

--- xadrez.c
+++ xadrez.c
@@ -28,5 +28,67 @@ int main() {
     // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
     // Inclua o uso de continue e break dentro dos loops.
 
+    const char bispo[5] = bispo , rainha[6]=rainha , torre[5]=torre;
+    int casas; 
+    const char esquerda[8]=esquerda , direita[7]=direita, cima[4]=cima, baixo[5]=baixo , diagonal[8]=diagonal;
+    const char diagonalSuperior[32]=diagonalSuperior, diagonalInferior[16]=diagonalInferior;
+    int opcao1; 
+    char opcao2[20] ,opcao3;
+
+    printf("Jogo de Xadrez:\n");
+    printf("Digite qual Peça Movimentar\n");
+    
+        do { 
+    printf("1. Bispo\n");
+    printf("2. Rainha\n");
+    printf("3. Torre\n");
+    printf("4. Sair\n");
+    printf("Opcao: ");
+    scanf("%d", &opcao1);
+
+    switch (opcao1)
+    {
+    case 1:
+        printf("Selecionado : Bispo\n");
+        
+        Printf("Digite a Direcao")
+
+
+
+        switch (opcao2)
+        {
+        case 1:
+            /* code */
+            break;
+        
+        default:
+            break;
+        }
+
+        break;
+    
+    case 2:
+       printf("Selecionado : Rainha\n");
+
+
+        break;
+
+    case 3:
+        printf("Selecionado : Torre\n");
+
+
+        break;
+
+
+    default:
+    printf("Opcao invalida");
+
+        break;
+    }
+
+
+
+
+
     return 0;
 }
