47a0ceb2c81f8a3aa09027fc88281498bd8742dc
GuhAlcantara <Gustavoamv@gmail.com>
2025-02-12 14:33:06 -0300

    Atualizar o xadrez.c
    add alguns comentarios

--- xadrez.c
+++ xadrez.c
@@ -7,28 +7,6 @@
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
 int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
-
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
-
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
-
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
-
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
-
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
-
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
 
 
     int bispo =  0 , rainha = 0 , torre = 0 ,  cavalo = 0 , j ; 
@@ -37,6 +15,8 @@ int main() {
     printf("Jogo de Xadrez:\n");
     printf("Digite qual Peça Movimentar\n");
     
+         // menu inicial para seleção das peças 
+
         do { 
     printf("1. Bispo\n");
     printf("2. Rainha\n");
@@ -121,8 +101,7 @@ default:
     break;
 }
 
-        } while (opcao1 != 0
-		);
+        } while (opcao1 != 0); // finalização do menu , sendo o 0(zero) a opção de sair no menu.
 
 
     return 0;
